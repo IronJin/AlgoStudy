701b97665d35f07007f4eeee47b2f8b13da0e849
Sanghyunn <shawnpark98@naver.com>
2021-08-25 17:11:42 +0900

    Day24

--- Day24/Princess_2457.cpp
+++ Day24/Princess_2457.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
-#include <vector>
+#include <deque>
+#include <algorithm>
 
 using namespace std;
 
-class Flower {
-	int s_month, s_day;
-	int e_month, e_day;
-	Date(int sm, int sd, int em, int ed) { s_month = sm; s_day = sd; e_month = em; e_day = ed;}
+class Date {
+ public :
+	int month;
+	int day;
+	Date() { month = 0; day = 0; }
+	Date(int m, int d) {month = m; day = d;}
+	bool operator == (Date& t) { return (month == t.month) && (day == t.day); }	
+	bool operator > (Date& t)
+	{
+		if(month == t.month)
+			return day > t.day;
+		return month > t.month;
+	}	
+
+	void print()
+	{
+		cout << "( " << month << ", " << day << ")" << endl;
+	}
 };
 
-int months[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-vector<Flower> v;
+class Flower {
+ public :
+	Date start;
+	Date end;
+	Flower(int sm, int sd, int em, int ed) 
+	{ 
+		if(sm < 3) { sm = 3; sd = 1;}
+		if(em > 11) { em = 12; ed = 1;}
+
+		start = Date(sm, sd);
+		end = Date(em, ed);
+	}
+};
+
+bool comp(Flower& a, Flower& b)
+{
+	if(a.end == b.end)
+		return b.start > a.start;
+
+	return a.end > b.end;	
+}
+	
+deque<Flower> d;
 
 int main()
 {
@@ -21,10 +57,52 @@ int main()
 	{
 		cin >> input1 >> input2 >> input3 >> input4;
 
-		if(input1 <= 2) {input1 = 3, input2 = 1;}
-		if(input3 >= 12) {input3 = 11, input4 = months[11];}
-		v.push_back(Date(input1, input2, input3, input4));
+		d.push_back(Flower(input1, input2, input3, input4));
 	}
+
+	sort(d.begin(), d.end(), comp);
+
+
+
+	int cnt = 0;
+	bool ps = false;
+	Date cutline = Date(12, 1), temp = cutline, f_day = Date(3, 1);
+
+	while(!ps && !d.empty())
+	{
+		if(cutline > d.front().end)
+			ps = true;
+
+		else
+		{
+			while(!d.empty())
+			{
+				if(d.front().end > cutline || d.front().end == cutline)
+				{
+					if(temp > d.front().start)
+						temp = d.front().start;
+
+					d.pop_front();
+				}
+
+				else break;
+			}
+
+			cutline = temp;
+
+			cnt++;
+			if(cutline == f_day) break;	
+		}		
+
+	}
+			
+	if(cutline == f_day) ps = false;
+	else ps = true;
+	
+	if(ps) cout << "0"; 
+	else cout << cnt; 
+
+	return 0;
 }
 
 		
